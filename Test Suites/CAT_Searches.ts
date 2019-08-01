69e9c4cc48808abb5cdab2d16d9a677d1c39616b
David Horne <David.horne@checkatrade.com>
2019-08-01 15:10:08 +0100

    First Iteration

---
+++ Test Suites/CAT_Searches.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Checkatrade - Trade and Location Searches</description>
+   <name>CAT_Searches</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7cecdfd8-e9ea-4f4f-85d9-1d08785476fb</testSuiteGuid>
+   <testCaseLink>
+      <guid>8e24ae62-5fb1-4ca6-9c27-577562bf4ac4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Checkatrade_Tradesman_Searches/Trade_Location_Search</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ac5ddb1d-2b02-4632-af1d-5f956b0f1b74</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
